ad25a3b98f1e8abf8634328ae89abf9bffa121b0
JSDALE <jdale994@googlemail.com>
2024-11-08 15:30:28 +0000

    added buzzer

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <Arduino.h>
 
 const int thermistorPin = A0;     // Analog pin used to connect the thermistor
+const int buzzerPin = 3;
 
 void setup() 
 {
   Serial.begin(9600);
   pinMode(thermistorPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() 
@@ -17,5 +19,12 @@ void loop()
   float temp = (voltage - 0.5) * 100;
   Serial.print("Temp C: ");
   Serial.println(temp);
-  delay(5000);
+  delay(2000);
+  if (temp > 65)
+  {
+    digitalWrite(buzzerPin, HIGH);
+    return;
+  }
+
+  digitalWrite(buzzerPin, LOW);
 }
