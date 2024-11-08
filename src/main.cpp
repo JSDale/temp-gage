c42bbb8682183d6a205099b6ac584715bd7db774
JSDALE <jdale994@googlemail.com>
2024-11-08 12:24:33 +0000

    using different sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 #include <Arduino.h>
 
 const int thermistorPin = A0;     // Analog pin used to connect the thermistor
-const int seriesResistor = 10000; // Resistance of the series resistor in Ohms
-const float nominalResistance = 10000; // Resistance at room temperature in Ohms
-const float tempNominal = 25;      // Room temperature in Celsius
-const float betaCoefficient = 3950; // Thermistor beta coefficient
 
 void setup() 
 {
@@ -12,35 +8,14 @@ void setup()
   pinMode(thermistorPin, INPUT);
 }
 
-float CalculateTemperature(float resistance) 
-{
-  float lnRt = log(resistance / nominalResistance);
-  Serial.print("resistance: ");
-  Serial.println(lnRt);
-  float temperature = 1 / (betaCoefficient * lnRt + 1 / (tempNominal + 273.15));
-  Serial.print("Kelvin: ");
-  Serial.println(temperature);
-  return temperature - 273.15; // Convert Kelvin to Celsius
-}
-
-float MyMap(float reading, int min, int max, int outMin, int outMax)
-{
-  auto inRange = max - min;
-  auto percentage = (reading / inRange) * 100;
-  auto outRange = outMax - outMin;
-  auto value = (percentage / 100) * outRange;
-  return value;
-}
-
 void loop() 
 {
-  int rawReading = analogRead(thermistorPin);
-  Serial.print("raw reading: ");
-  Serial.println(rawReading);
-  int resistance = MyMap(rawReading, 0, 1023, 0, seriesResistor);
-  float temperature = CalculateTemperature(resistance);
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.println("°C");
+  float input = analogRead(thermistorPin);
+  float voltage = (input/1024.0) * 5.0;
+  Serial.print("Voltage: ");
+  Serial.println(voltage);
+  float temp = (voltage - 0.5) * 100;
+  Serial.print("Temp C: ");
+  Serial.println(temp);
   delay(5000);
 }
