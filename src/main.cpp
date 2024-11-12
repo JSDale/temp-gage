9a3f267128d7d9017557a106c87b8d021fa72bad
JSDALE <jdale994@googlemail.com>
2024-11-12 13:12:39 +0000

    added LCD

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,29 @@
 #include <Arduino.h>
+#include <LiquidCrystal.h>
 
 // Thermistor parameters from the datasheet
 #define RT0 100000
 #define B 3950
 
 // Our series resistor value = 10 kΩ
-#define R 10000  
+#define R 10000
 
 // Variables for calculations
 float RT, VR, ln, TX, T0, VRT;
 
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
 void setup() {
-  // Setup serial communication
   Serial.begin(9600);
+  lcd.begin(16, 2);
   // Convert T0 from Celsius to Kelvin
   T0 = 25 + 273.15;
+  lcd.print("hello");
+  delay(1000);
 }
 
 void loop() {
+  lcd.clear();
   // Read the voltage across the thermistor
   VRT = (5.00 / 1023.00) * analogRead(A0);
   Serial.print("voltage on thermistor: ");
@@ -44,6 +50,7 @@ void loop() {
   // Display in Celsius
   Serial.print(TX);                  
   Serial.print("C\t");
+  lcd.print(String(TX) + " C");
   
-  delay(5000);
+  delay(2000);
 }
