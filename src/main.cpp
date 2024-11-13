1f82d3dc00543d4c28d5405422f131ceefff5075
JSDALE <jdale994@googlemail.com>
2024-11-13 19:33:28 +0000

    added buzzer, trying to add button

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,52 @@
 // Variables for calculations
 float RT, VR, ln, TX, T0, VRT;
 
+const int BuzzPin = 8;
+const int LimitPin = 10;
+int limitIndex = 0;
+int limits[] = {50, 65, 85};
+
+unsigned long previousM = millis();
+
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
+  pinMode(BuzzPin, OUTPUT);
+  pinMode(LimitPin, INPUT);
   // Convert T0 from Celsius to Kelvin
   T0 = 25 + 273.15;
   lcd.print("hello");
   delay(1000);
 }
 
+void PlayTone()
+{
+  auto m = millis();
+  if ((m - previousM) > 1000)
+  {
+    tone(BuzzPin, 262);
+  }
+  else
+  {
+    tone(BuzzPin, 349);
+  }
+  previousM = m;
+}
+
+void StopTone()
+{
+  noTone(BuzzPin);
+}
+
 void loop() {
-  lcd.clear();
+  if (digitalRead(LimitPin) == HIGH)
+  {
+    limitIndex++;
+    if (limitIndex > 3) limitIndex = 0;
+  }
+
   // Read the voltage across the thermistor
   VRT = (5.00 / 1023.00) * analogRead(A0);
   Serial.print("voltage on thermistor: ");
@@ -50,7 +83,19 @@ void loop() {
   // Display in Celsius
   Serial.print(TX);                  
   Serial.print("C\t");
+  lcd.clear();
   lcd.print(String(TX) + " C");
+  lcd.setCursor(0, 2);
+  lcd.print("Warning at: " + String(limits[limitIndex]));
+
+  if (TX > limits[limitIndex])
+  {
+    PlayTone();
+  }
+  else
+  {
+    StopTone();
+  }
   
   delay(2000);
 }
